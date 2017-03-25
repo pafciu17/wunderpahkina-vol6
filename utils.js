1b2c781e7c0aada6f157d5d08e94a86c9ae0581a
pawel <pawel.niechoda@gmail.com>
2017-03-25 11:35:21 +0200

    create utils.js

---
+++ utils.js
@@ -0,0 +1,31 @@
+const R = require('ramda')
+
+const getBiggestDimension = (index, currentMin, currentValue) => {
+  if (currentMin > currentValue) {
+    return R.max(currentValue, index)
+  } else {
+    return R.min(currentMin, currentValue)
+  }
+}
+
+const getSquareStaringAtIndex0 = input => {
+  const { min } = R.reduce((a, b) => ({
+    index: a.index + 1,
+    min: getBiggestDimension(a.index, a.min, b)
+  }), {
+    index: 0,
+    min: Infinity
+  }, input)
+
+  if (min > input.length) {
+    return input.length
+  } else {
+    return min
+  }
+}
+
+const calculateSquareAreaStartingAtIndex0 = input => Math.pow(getSquareStaringAtIndex0(input), 2)
+
+module.exports = {
+  calculateSquareAreaStartingAtIndex0
+}
