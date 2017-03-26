20246b8e6695a1e929c959cbbfa509831df15f64
pawel <pawel.niechoda@gmail.com>
2017-03-26 20:33:55 +0300

    add top level of solving the problem

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 const fs = require('fs')
+const R = require('ramda')
+const { calculateSquareAreaStartingAtIndex0 } = require('./utils')
 
-const handleSolution = (input) => {
-  for(i in input) {
-    console.log(input[i])
+const findMax = R.reduce(R.max, 0)
+
+const solve = input => {
+  const calculateSizePerSection = (element, index) => {
+    const section = R.slice(index, index + element, input)
+    return calculateSquareAreaStartingAtIndex0(section)
   }
+  return R.compose(
+    findMax,
+    R.addIndex(R.map)(calculateSizePerSection)
+  )(input)
 }
 
 fs.readFile('input.txt', (err, data) => {
   if (err) {
     console.error(err)
   } else {
-    const input = data.toString().split("\n")
-    handleSolution(input)
+    const input = R.map(parseInt, data.toString().split("\n"))
+    const result = solve(input)
+    console.log(`Maximal size: ${result}`)
   }
 })
